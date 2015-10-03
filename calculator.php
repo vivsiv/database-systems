8e15d8ac87a76265aada1eb3982d4093f9e760dc
Vivek <vivek.a.sivakumar@gmail.com>
2015-10-03 16:31:19 -0700

    HW 1a first commit
    php calculator

---
+++ calculator.php
@@ -0,0 +1,44 @@
+<h1>Calculator</h1>
+<p>by Vivek Sivakumar</p>
+<br/>
+
+<p>
+<form action="calculator.php" method="GET">
+	<input type="text" name="expr">
+	<input type="submit" value="Calculate">
+</form>
+</p>
+
+<?php
+	if($_GET["expr"]){
+		$expr = $_GET["expr"];
+		// echo "expr: " . $expr;
+		// echo "<br/>";
+		$validExprRegex = '/([0-9](\+|\-|\*|\/))+[0-9]/';
+		preg_match($validExprRegex, $expr, $exprMatches);
+		// echo "exprMatches[0]: " . $exprMatches[0];
+		// echo "<br/>";
+		$validExpr = $exprMatches[0];
+		if ($validExpr){
+			$divideByZeroRegex = "/(\/0)/";
+			preg_match($divideByZeroRegex, $validExpr, $divideMatches);
+			$divideByZeroError = $divideMatches[0];
+			if ($divideByZeroError){
+				echo "Divide by zero error!";
+			}
+			else {
+				eval("\$answer = $expr;");
+				echo $expr . " = " . $answer;
+			}	
+		}
+		else {
+			echo "Invalid Expression!";
+		}
+
+		
+	}
+	else {
+		echo "Nothing to calculate";
+	}
+
+?>
