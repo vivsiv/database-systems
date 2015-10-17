a7c9d3744c37bde4f80b3c2003d4c074513097f7
Vivek <vivek.a.sivakumar@gmail.com>
2015-10-16 21:43:11 -0700

    queries.sql

---
+++ P1B/queries.sql
@@ -0,0 +1,31 @@
+-- Names of all actors in "Die Another Day"
+SELECT first, last
+FROM Actor
+WHERE Actor.id IN (
+	SELECT aid
+	FROM Movie, MovieActor
+	WHERE Movie.id = MovieActor.mid AND Movie.title = "Die Another Day"
+);
+
+
+-- Count of all actors who acted in multiple movies
+SELECT COUNT(*)
+FROM (
+	SELECT *
+	FROM MovieActor
+	GROUP BY aid
+	HAVING COUNT(mid) >= 2
+) AS M;
+
+-- Find Tom Hanks Rom Coms
+SELECT DISTINCT title
+FROM Movie, MovieGenre
+WHERE Movie.id = MovieGenre.mid AND
+Movie.id IN (
+	SELECT mid
+	FROM Actor, MovieActor
+	WHERE Actor.id = MovieActor.aid AND
+	Actor.first="Tom" AND 
+	Actor.last="Hanks"
+) AND (MovieGenre.genre="Romance" OR MovieGenre.genre="Comedy");
+
