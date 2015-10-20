a7e969891ad2e09b8b8e58f970495a2c213877e9
Vivek <vivek.a.sivakumar@gmail.com>
2015-10-19 20:06:15 -0700

    descriptions of constraints

--- P1B/create.sql
+++ P1B/create.sql
@@ -7,6 +7,8 @@ CREATE TABLE Movie (
 	PRIMARY KEY (id),
 	CHECK (year > 0 AND year <= 2015)
 ) ENGINE=INNODB;
+-- Making id the primary key for movies (it must be unique for all movies)
+-- Checking that all movie years are valid
 
 CREATE TABLE Actor (
 	id INT,
@@ -18,6 +20,8 @@ CREATE TABLE Actor (
 	PRIMARY KEY (id),
 	CHECK (dob <= 2015)
 ) ENGINE=INNODB;
+-- Making id the primary key for actors (it must be unique for all actors)
+-- Checking that all actor dates of birth are valid
 
 CREATE TABLE Director (
 	id INT,
@@ -28,12 +32,15 @@ CREATE TABLE Director (
 	PRIMARY KEY (id),
 	CHECK (dob <= 2015)
 ) ENGINE=INNODB;
+-- Making id the primary key for directors (it must be unique for all directors)
+-- Checking that all director dates of births are valid
 
 CREATE TABLE MovieGenre (
 	mid INT,
 	genre VARCHAR(20),
 	FOREIGN KEY (mid) REFERENCES Movie(id)
 ) ENGINE=INNODB;
+-- Each movie in the MovieGenre table refers to a movie in the Movie table
 
 CREATE TABLE MovieDirector (
 	mid INT,
@@ -41,6 +48,8 @@ CREATE TABLE MovieDirector (
 	FOREIGN KEY (mid) REFERENCES Movie(id),
 	FOREIGN KEY (did) REFERENCES Director(id)
 ) ENGINE=INNODB;
+-- Each movie in the MovieDirector table refers to a movie in the Movie table
+-- Each director in the MovieDirector table refers to a director in the Director table
 
 CREATE TABLE MovieActor (
 	mid INT,
@@ -50,6 +59,8 @@ CREATE TABLE MovieActor (
 	FOREIGN KEY (aid) REFERENCES Actor(id)
 
 ) ENGINE=INNODB;
+-- Each movie in the MovieActor table refers to a movie in the Movie table
+-- Each director in the MovieActor table refers to an actor in the Actor table
 
 CREATE TABLE Review (
 	name VARCHAR(20),
@@ -60,6 +71,7 @@ CREATE TABLE Review (
 	FOREIGN KEY (mid) REFERENCES Movie(id),
 	CHECK (rating >= 0 AND rating <= 5)
 ) ENGINE=INNODB;
+-- Each movie in the Review table refers to a movie in the Movie table
 
 CREATE TABLE MaxPersonID (
 	id INT
