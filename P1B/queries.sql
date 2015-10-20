54c4bba37567f621929769f5f75293f4b8864596
Vivek <vivek.a.sivakumar@gmail.com>
2015-10-19 19:06:21 -0700

    explanations for queries

--- P1B/queries.sql
+++ P1B/queries.sql
@@ -1,4 +1,6 @@
--- Names of all actors in "Die Another Day"
+-- To find all the actors in Die another day I joined the Movie and MovieActor tables to get the set of aid's in Die Another Day.
+-- Then I went through each actor in the Actor table to see if they were in this set, I selected the actors that were and printed
+-- their first and last names concatenated together
 SELECT CONCAT(first, " ", last) AS "Actors in Die Another Day"
 FROM Actor
 WHERE Actor.id IN (
@@ -7,7 +9,8 @@ WHERE Actor.id IN (
 	WHERE Movie.id = MovieActor.mid AND Movie.title = "Die Another Day"
 );
 
--- Count of all actors who acted in multiple movies
+-- To get the number of actors in multiple movies I first grouped entries in the MovieActor table by aid, I then selected those groups
+-- that had 2 or mid's associated with them. Finally I counted the number of groups I had.
 SELECT COUNT(*) AS "Number of Actors in multiple movies"
 FROM (
 	SELECT *
@@ -16,7 +19,9 @@ FROM (
 	HAVING COUNT(mid) >= 2
 ) AS M;
 
--- Find All Meg Ryan RomComs
+-- To find all Meg Ryan RomComs I joined the Actor and MovieActor tables to get the set of mid's with Meg Ryan in them.
+-- I then joined the Movie and Movie Genre tables and selected those tuples that were in the set of mid's with meg ryan
+-- and the set of movies with genre Romance or Comedy
 SELECT DISTINCT title AS "Meg Ryan RomComs"
 FROM Movie, MovieGenre
 WHERE Movie.id = MovieGenre.mid AND
