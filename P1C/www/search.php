0982625b68bc7a813197d36400f19a6f8cbdb001
Vivek <vivek.a.sivakumar@gmail.com>
2015-10-27 23:16:51 -0700

    clean up search

--- P1C/www/search.php
+++ P1C/www/search.php
@@ -2,17 +2,15 @@
 	include 'db_functions.php';
 	if($_GET["search"]){
 		$search = $_GET["search"];
-		print "<b>Searched..</b>: '" . $search . "'";
+		print "<b>Searched:</b> '" . $search . "'";
 		print "<br/><br/>";
 		$db_connection = create_connection("localhost", "cs143", "");
-		print $db_connection->host_info;
-		print "<br/>";
-		$db_selected = choose_db("TEST", $db_connection);
+		//$db_selected = choose_db("TEST", $db_connection);
 		$sanitized_search = sanitize_string($search, $db_connection);
 	
 		$searched_movie = sprintf("%s", $sanitized_search);
 		$movie_query = "select * from Movie where title like '%$searched_movie%'";
-		print $movie_query . "<br/>";
+		//print $movie_query . "<br/>";
 
 		$actor_names = explode(" ", $sanitized_search);
 		$actor_base_query = "select * from Actor where first='%s'";
@@ -22,7 +20,7 @@
 			$actor_base_query .= " and last='%s'";
 			$actor_query = sprintf($actor_base_query, $actor_names[0], $actor_names[1]);
 		}
-		print $actor_query . "<br/>";
+		//print $actor_query . "<br/>";
 		
 		$movies = run_query($movie_query, $db_connection);
 		$actors = run_query($actor_query, $db_connection);
