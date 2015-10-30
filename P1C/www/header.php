70b6a19862eee8de1f4d364e21a4d2dc0903adf0
Vivek <vivek.a.sivakumar@gmail.com>
2015-10-29 20:19:33 -0700

    move search box to header

--- P1C/www/header.php
+++ P1C/www/header.php
@@ -3,4 +3,12 @@
 	<h1 class="title">Vivek's Movie Database</h1>
 	<p class='header_link'><a href="index.php">Home</a></p>
 	<hr/>
+
+	<h2>Search for Actor or Movie</h2>
+	<form id="search_form" action='search.php' method='GET'>
+		<textarea id='search' name='search'></textarea>
+		<br/><br/>
+		<input id='submit' type='submit' value='Search'/>
+	</form>
+	<hr/>
 </head>
--- P1C/www/index.php
+++ P1C/www/index.php
@@ -2,14 +2,6 @@
 <html>
 	<?php include 'header.php'; ?>
 	<body>
-		<h2>Search for Actor or Movie</h2>
-		<form id="search_form" action='search.php' method='GET'>
-			<textarea id='search' name='search'></textarea>
-			<br/><br/>
-			<input id='submit' type='submit' value='Search'/>
-		</form>
-		<hr/>
-
 		<h2>Create Actor/Director</h2>
 		<form id="create_person_form" action='create_person.php' method='GET'>
 			<select id="create_person_select" name='create_person_select'>
