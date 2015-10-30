02a3be9d8c6c608e7ce76c57bec3cd35afd9b541
Vivek <vivek.a.sivakumar@gmail.com>
2015-10-29 23:29:09 -0700

    snazz up director browse

--- P1C/www/director.php
+++ P1C/www/director.php
@@ -7,25 +7,23 @@
 		$director_id = intval(sanitize_string($_GET["director_id"], $db_connection));
 		$director_base_query = "select * from Director where id=%d";
 		$director_query = sprintf($director_base_query, $director_id);
-		print $director_query . "<br/>";
+		//print $director_query . "<br/>";
 		$director = run_query($director_query, $db_connection);
 
 		$director_attr = mysql_fetch_array($director, MYSQL_ASSOC);
-		print "<ul>";
-		printf("<li>%s %s</li>", $director_attr["first"], $director_attr["last"]);
-		printf("<li>%s</li>", $director_attr["dob"]);
-		print "</ul>";
-		print "<hr/>";
+		$dob_year = explode("-", $director_attr["dob"])[0];
+		$dod_year = explode("-", $director_attr["dob"])[0];
+		printf("<h2 class='page_header'>%s %s (%s - )</h2>", $director_attr["first"], $director_attr["last"], $dob_year, $dod_year);
+		printf("<h4 class='page_centered'>%s %s</h4>", $director_attr["rating"], $director_attr["company"]);
 
 		$movie_director_base_query = "select mid from MovieDirector where did=%d";
 		$movie_director_query = sprintf($movie_director_base_query, $director_id);
-		print $movie_director_query . "<br/>";
+		//print $movie_director_query . "<br/>";
 		$movie_directors = run_query($movie_director_query, $db_connection);
 
-		print "<p>Movies..</p>";
 		$url_safe_director_name = $director_attr["first"] . "+" . $director_attr["last"];
 		$movie_director_link = sprintf("movie_director_form.php?director_id=%d&director_name=%s", $director_id, $url_safe_director_name);
-		print "<p><a href=$movie_director_link>Add a Movie for this Director</a></p>";
+		print "<h2>Movies <a href=$movie_director_link>(add)</a></h2>";
 		if (mysql_num_rows($movie_directors) > 0){
 			$mids = array();
 			while ($movie_director_row = mysql_fetch_array($movie_directors, MYSQL_ASSOC)){
@@ -34,7 +32,7 @@
 			$mid_string = implode(",",$mids);
 			$movie_base_query = "select * from Movie where id in (%s)";
 			$movie_query = sprintf($movie_base_query, $mid_string);
-			print $movie_query . "<br/>";
+			//print $movie_query . "<br/>";
 			$movies = run_query($movie_query, $db_connection);
 
 			print "<ul>";
@@ -43,7 +41,6 @@
 				printf("<li><a href=%s>%s (%s) Rated: %s</a></li>", $movie_link, $movie_row["title"], $movie_row["year"], $movie_row["rating"]);
 			}
 			print "</ul>";
-			print "<hr/>";
 		}
 		close_connection($db_connection);
 	}
