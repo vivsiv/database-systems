89282fe377cc7d40c23db47525c3d540ec3b4dd5
Vivek <vivek.a.sivakumar@gmail.com>
2015-10-29 19:40:30 -0700

    make add movie to actor available even if no movies

--- P1C/www/actor.php
+++ P1C/www/actor.php
@@ -21,6 +21,10 @@
 		print $movie_actor_query . "<br/>";
 		$movie_actors = run_query($movie_actor_query, $db_connection);
 
+		print "<p>Movies..</p>";
+		$url_safe_actor_name = $actor_attr["first"] . "+" . $actor_attr["last"];
+		$movie_actor_link = sprintf("movie_actor_form.php?actor_id=%d&actor_name=%s", $actor_id, $url_safe_actor_name);
+		print "<p><a href=$movie_actor_link>Add a Movie for this Actor</a></p>";
 		if (mysql_num_rows($movie_actors) > 0){
 			$mids = array();
 			$roles = array();
@@ -34,15 +38,14 @@
 			print $movie_query . "<br/>";
 			$movies = run_query($movie_query, $db_connection);
 
-			print "<p>Movies..</p>";
 			print "<ul>";
 			while ($movie_row = mysql_fetch_array($movies, MYSQL_ASSOC)){
 				$movie_link = sprintf("movie.php?movie_id=%d", intval($movie_row['id']));
 				printf("<li><a href='%s'>%s as %s (%s) Rated: %s</a></li>", $movie_link, $movie_row["title"], $roles[$movie_row["id"]], $movie_row["year"], $movie_row["rating"]);
 			}
 			print "</ul>";
-			print "<hr/>";
 		}
+		print "<hr/>";
 		close_connection($db_connection);
 	}
 
