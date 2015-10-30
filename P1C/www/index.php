1a88482ab55ccd25e1d90f02874b5752e6a62aaf
Vivek <vivek.a.sivakumar@gmail.com>
2015-10-29 18:21:39 -0700

    remove review form from index page

--- P1C/www/index.php
+++ P1C/www/index.php
@@ -68,22 +68,6 @@
 			<br/><br/>
 			<input id='submit' type='submit' value='Create Movie Person'/>
 		</form>
-		<hr/>
-
-		<h2>Add Review</h2>
-		<form id="create_review" action="create_review.php" method="GET">
-			<input name="create_review" type="hidden" value="true"/>
-			<h4>Movie id</h4>
-			<input type="text" name="movie_id"/>
-			<h4>Name</h4>
-			<input type="text" name="review_name"/>
-			<h4>Rating (1-5)</h4>
-			<input type="text" name="rating"/>
-			<h4>Comment</h4>
-			<textarea type="text" name="comment"></textarea>
-			<br/><br/>
-			<input id='submit' type='submit' value='Create Movie Person'/>
-		</form>
 
 
 	<?php
