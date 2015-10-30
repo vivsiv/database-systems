e21d44834dcd14850d1138665445905f2425662a
Vivek <vivek.a.sivakumar@gmail.com>
2015-10-29 22:13:03 -0700

    create_person bug fixes

--- P1C/www/create_person.php
+++ P1C/www/create_person.php
@@ -11,29 +11,27 @@
 		$id = $old_max_person_id + 1;
 		$first = sanitize_string($_GET["first"], $db_connection);
 		$last = sanitize_string($_GET["last"], $db_connection);
-		$sex = sanitize_string($_GET["sex"], $db_connection);
-		$dob = intval(sanitize_string($_GET["dob"], $db_connection));
+		$sex = NULL;
+		if ($_GET["sex"]) $sex = sanitize_string($_GET["sex"], $db_connection);
+		$dob = sanitize_string($_GET["dob"], $db_connection);
 		$dod = NULL;
-		if ($_GET["dod"]){
-			$dod = intval(sanitize_string($_GET["dod"], $db_connection));
-		}
+		if ($_GET["dod"]) $dod = sanitize_string($_GET["dod"], $db_connection);
 
-		$person_type = sanitize_string($_GET["create_person_select"], $db_connection);
+		$person_type = sanitize_string($_GET["person_type"], $db_connection);
 		print $person_type . "<br/>";
 		if ($person_type == "actor"){
 			$actor_base_query = "insert into Actor values (%d,'%s','%s','%s','%s','%s')";
-			$actor_query = sprintf($actor_base_query, $id, $first, $last, $sex, $dob, $dod);
+			$actor_query = sprintf($actor_base_query, $id, $last, $first, $sex, $dob, $dod);
 			print $actor_query . "<br/>";
 			run_query($actor_query, $db_connection);
 		}
 		else if ($person_type == "director"){
 			$director_base_query = "insert into Director values (%d,'%s','%s','%s','%s')";
-			$director_query = sprintf($director_base_query, $id, $first, $last, $dob, $dod);
+			$director_query = sprintf($director_base_query, $id, $last, $first, $dob, $dod);
 			print $director_query . "<br/>";
 			run_query($director_query, $db_connection);
 		}
 
-		
 		$max_person_update_query = sprintf("update MaxPersonID set id=%d where id=%d", $id, $old_max_person_id);
 		print $max_person_update_query . "<br/>";
 		run_query($max_person_update_query, $db_connection);
